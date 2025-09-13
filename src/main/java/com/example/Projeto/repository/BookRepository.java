136185a2a199f4378fb8c208e3a272cdd10217df
HeadPrimeOficial <headtutoriall@gmail.com>
2025-09-13 20:57:52 -0300

    ok

--- src/main/java/com/example/Projeto/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Projeto.repository;
-
-import com.example.Projeto.model.Book;
-import com.example.Projeto.service.BookService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {}
